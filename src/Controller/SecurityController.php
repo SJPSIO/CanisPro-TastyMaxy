a0949d1882ef220711f7c19d6b219e998dc5e5b5
Portable User <user@example.com>
2026-03-18 15:00:45 +0100

    PTNNNN CA MARCHE Y JVAIS LE BZ SA TANTE

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SecurityController extends AbstractController
 {
     #[Route('/connexion', name: 'app_login')]
-    public function login(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/login.html.twig');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        
+        return $this->render('security/login.html.twig',[
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
     #[Route('/deconnexion', name: 'app_logout')]
